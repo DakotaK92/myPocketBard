585718f4f1bc6c77b6ecd4dd2c2db155cf4de0bc
DakotaK92 <46942706+DakotaK92@users.noreply.github.com>
2024-12-11 10:53:22 -0600

    Update Intro

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import "./intro.css";
+import iPhone from "../../assets/images/iPhone.png";
+import appleStore from "../../assets/images/Apple_Store.png";
+import googlePlay from "../../assets/images/Google_Play.png";
 
 const Intro = () => {
     return (
         <div className="intro">
             <div className="introContainer">
-                <h1 className="introTitle">The bard that fits in your pocket</h1>
-                <p className="introPara">Whether you need epic orchestral music for a fierce dragon
-                    battle or calming forest sounds for a peaceful evening read,
-                    Pocket Bard has you covered.
-                </p>
+                <div className="introInfo">
+                    <h1 className="introTitle">The bard that fits in your pocket</h1>
+                    <p className="introPara">Whether you need epic orchestral music for a fierce dragon<br/>
+                        battle or calming forest sounds for a peaceful evening read,<br/>
+                        Pocket Bard has you covered.
+                    </p>
+                    <div className="introBtns">
+                        <img src={googlePlay} alt="googlePlay" className="googlePlay"/>
+                        <img src={appleStore} alt="appleStore" className="appleStore"/>
+                    </div>
+                </div>
+                <img src={iPhone} alt="iPhone" className="iPhone"/>
             </div>
             
         </div>
